461163d08e00d4cc8cd4bcdf2aade49766cbf1a9
hurricane1988 <hurricane_1988@163.com>
2024-04-13 19:11:44 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,14 @@
-use std::path::PathBuf;
-use clap::{Parser, Subcommand};
+mod cpu;
+mod user;
+mod process;
 
-/// TODO: https://docs.rs/clap/latest/clap/_derive/_tutorial/chapter_0/index.html
+use crate::user::user_info::{list_users};
+use crate::process::process_info::{process_summary};
+use crate::cpu::cpu_info::{cpu_usage, physical_core_count};
 
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
-
-    /// Set a custom config file.
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
-    /// Turn debugging information on.
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// does testing things.
-    Test {
-        /// lists test values.
-        #[arg(short, long)]
-        list: bool,
-    },
-}
 
 fn main() {
-    let cli = Cli::parse();
-
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {name}");
-    }
-
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display())
-    }
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode if kind of on"),
-        2 => println!("Debug mode if on"),
-        _ => println!("Don't be crazy"),
-    }
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    match &cli.command {
-        Some(Commands::Test {list}) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...")
-            }
-        }
-        None => {}
-    }
+    // list_users();
+    // process_summary();
+    physical_core_count()
 }
